5f3a75c5426a8f9e0de50275252ef85eefaaf230
Florian Kattner <florian.kattner@gmail.com>
2022-07-17 10:14:27 +0200

    Add clear button; display the numbers clicked

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
-const buttons = document.querySelectorAll(".buttons .button");
+
+const buttons = document.querySelectorAll(".buttons .button, .display .clear");
 const resultDisplay = document.querySelector(".display .result");
+console.log(buttons);
 
 function add(a, b){
     return a + b;
@@ -40,10 +42,16 @@ function clickButton(dataValue){
         case "multiply": break;
         case "add": break;
         case "subtract": break;
+        case "clear": clearDisplay(); break;
         default: break;
     }
 
     if(!isNaN(dataValue)) {
-        resultDisplay.textContent = dataValue;
+        if(resultDisplay.textContent === "0") resultDisplay.textContent = "";
+        resultDisplay.textContent += dataValue;
     }
+}
+
+function clearDisplay(){
+    resultDisplay.textContent = 0;
 }
